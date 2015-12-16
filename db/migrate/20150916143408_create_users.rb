ae94168d97652567f4dad8909e0cf7a5e8f88aae
sammarcus <sam.marcus@me.com>
2015-12-16 01:46:52 -0500

    dual migration issue, leftover db

--- db/migrate/20150916143408_create_users.rb
+++ db/migrate/20150916143408_create_users.rb
@@ -1,13 +1,22 @@
 class CreateUsers < ActiveRecord::Migration
   def up
-  	create_table :users do |t|
-  		t.string :username
-  		t.string :email
-  		t.string :fullname
-  	end
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :fullname
+    end
   end
 
   def down
-  	drop_table :users
+    drop_table :users
+  end
+
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :fav_icecream
+    end
+
   end
 end

f436995f241c73a99408826e6b21147cb43e8e83
vicfriedman <victoria.m.friedman@gmail.com>
2015-12-16 09:57:58 -0500

    db error

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117223101) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "fav_icecream"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end

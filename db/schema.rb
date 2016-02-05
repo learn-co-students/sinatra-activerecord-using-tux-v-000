1e38cbc6dca1a2d85ca783562f1b19869c9e82cd
macgeek30 <mike25ko@gmail.com>
2016-02-05 23:12:45 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117223101) do
+ActiveRecord::Schema.define(version: 20160205230822) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string  "content"
+    t.string  "title"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"

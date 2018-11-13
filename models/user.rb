d9fd4f2cadc9df17a507468c56a674ab7cbda4bd
brandong1 <brandon@brandonleeproductions.com>
2018-11-12 18:35:42 -0700

    Done.

--- models/user.rb
+++ models/user.rb
@@ -4,3 +4,12 @@ class User < ActiveRecord::Base
     "my name is #{self.name}"
   end
 end
+
+
+#####NO IDEA WHY THIS TEST ISN'T PASSING#####
+
+#####GETTING THIS ERROR#####
+
+#[18:33:58] (master) sinatra-activerecord-using-tux-v-000
+#// ♥ objc[70066]: +[__NSPlaceholderDictionary initialize] may have been in progress in another thread when fork() was called.
+#objc[70066]: +[__NSPlaceholderDictionary initialize] may have been in progress in another thread when fork() was called. We cannot safelycall it or ignore it in the fork() child process. Crashing instead. Set a breakpoint on objc_initializeAfterForkError to debug.
